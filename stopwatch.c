fe91e3be0845a32c8a88ddd29999d90474b5d8d7
fatimahamid813 <fatimahamid813@gmail.com>
2025-06-15 00:29:15 +0500

    display enhanced

--- stopwatch.c
+++ stopwatch.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
-#include <windows.h> //for sleep funtion
+#include <windows.h> //for sleep funtion abd colors and clearing screen
 #include <conio.h> //for _kbhit and _getch 
+void display (int hours, int minutes, int seconds, int isRunning) 
+{
+    system("cls");// Clear the console screen
 
-void displayTime(int h, int m, int s)
- {
-    printf("\r %02d: %02d: %02d", h, m, s); // \r brings the cursor to beginning
-    fflush(stdout); // flush output to update same line
+    HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);//give the acces to console for changing color 
+
+
+    SetConsoleTextAttribute(screen, 5);// Set color of the  title
+    printf("============================\n");
+    printf("      DIGITAL STOPWATCH     \n");
+    printf("============================\n");
+
+    SetConsoleTextAttribute(screen, 13); // Set color for the timer
+    printf("\n          %02d:%02d:%02d\n", hours, minutes, seconds);
+
+    SetConsoleTextAttribute(screen, 5);// Set color for the status
+    if (isRunning) {
+        printf("\n      Status: RUNNING\n");
+    } else {
+        printf("\n      Status: PAUSED\n");
+    }
+
+    SetConsoleTextAttribute(screen, 13);// Set color for the controls
+
+    printf("\nControls: [S] Start/Stop\n          [R] Reset \n          [E] Exit\n");
 }
+
+// void displayTime(int h, int m, int s)
+//  {
+//     printf("\r %02d: %02d: %02d", h, m, s); // \r brings the cursor to beginning
+//     fflush(stdout); // flush output to update same line
+// }
 
 int main() 
 {
@@ -19,7 +45,7 @@ int main()
     printf("S = Start/Stop\n");
     printf("R = Reset\n");
     printf("E = Exit\n\n");
-    displayTime(hours, minutes, seconds);
+    display(hours, minutes, seconds, running);
 
     while (1) {
         if (_kbhit()) //If a key is pressed, get that key and store it in the variable key so we can use
@@ -28,12 +54,13 @@ int main()
             if (key == 's' || key == 'S') 
             {
                 running = !running;
+                display(hours, minutes, seconds, running);
             } 
             else if (key == 'r' || key == 'R') 
             {
                 hours = minutes = seconds = 0;
                 running = 0;
-                displayTime(hours, minutes, seconds);
+                display(hours, minutes, seconds, running);
             }
              else if (key == 'e' || key == 'E') 
              {
@@ -57,7 +84,7 @@ int main()
                 hours++;
             }
 
-            displayTime(hours, minutes, seconds);
+            display(hours, minutes, seconds, running);
         } 
         else
          {
